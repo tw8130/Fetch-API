ff7a8e62a3e4b4a5384fd680853724d40e328365
tw8130 <mwangitabitha2020@gmail.com>
2023-05-19 11:19:09 +0300

    updated the files and added http reqeust methods like put,delete,post and filter

---
+++ index.js
@@ -0,0 +1,89 @@
+//fetchAPI
+// const fetch = require('node-fetch');
+//making http requests to list all the posts results
+
+const posts_url = "https://jsonplaceholder.typicode.com/posts";
+
+// fetch(posts_url).then(res => res.json().then(results => console.log(results)));
+
+
+// async function getPosts() {
+//     let result = await fetch(posts_url);
+//     console.log(result);
+//     let posts = await result.json()
+//     console.log(posts);
+// }
+
+// getPosts()
+
+
+//HTTPS REQUEST TO POST 
+const new_post = {
+    title: 'foo',
+    body: 'bar',
+    userId: 1,
+}
+
+async function postPosts() {
+    let res = await fetch(posts_url, {
+        method: 'POST',
+        body: JSON.stringify(new_post)
+    })
+
+
+    let results = await res.json()
+    console.log(results);
+}
+
+postPosts();
+
+//HTTP REQUEST TO UPDATE POST
+const posts_url2 = "https://jsonplaceholder.typicode.com/posts/1";
+let posts1 = {
+    id: 1,
+    title: 'foo,foo',
+    body: 'bar',
+    userId: 1,
+};
+
+
+// fetch(`${posts_url}/1`, {
+//     method: 'PUT',
+//     body: posts1
+// });
+
+async function putPosts() {
+    let res = await fetch(posts_url2, {
+        method: 'PUT',
+        body: JSON.stringify(posts1)
+    })
+
+
+    let results2 = await res.json()
+    console.log(results2);
+}
+
+putPosts();
+
+//HTTP REQUEST TO DELETE A POST
+
+async function deletePosts() {
+
+    let res = await fetch(posts_url2, {
+        method: 'DELETE',
+    })
+};
+deletePosts();
+
+
+//HTTP REQUEST TO FILTER A POST
+const posts_url3 = "https://jsonplaceholder.typicode.com/posts?userId=1";
+async function filterPosts() {
+
+    let res = await fetch(posts_url3, {
+        method: 'GET',
+    })
+    let results = await res.json()
+    console.log(results);
+}
+filterPosts();
